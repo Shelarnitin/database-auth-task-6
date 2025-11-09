272debdeeb0c152277126d0c5318a0b3b405872b
Shelarnitin <shelarnitin399@gmail.com>
2025-11-09 17:59:39 +0530

    User schema for DB

--- models/User.js
+++ models/User.js
@@ -1,20 +1,9 @@
-import { Schema, model } from "mongoose";
+const mongoose = require("mongoose");
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-},{timeseries: true});
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email:    { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+}, { timestamps: true });
 
-export default model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
