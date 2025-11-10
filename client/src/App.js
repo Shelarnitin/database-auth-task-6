06764154aa99b7961cfc2853d2817abd9a039180
Shelarnitin <shelarnitin399@gmail.com>
2025-11-10 21:49:10 +0530

    Froent End

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+// client/src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <h2>User Authentication</h2>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
